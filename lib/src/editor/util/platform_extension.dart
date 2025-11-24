8b08826bdee9ffd6d57f0205297625bd9e6ee7e8
DexterZP <andredexter@live.com>
2025-11-24 12:53:18 -0300

    ...

--- lib/src/editor/util/platform_extension.dart
+++ lib/src/editor/util/platform_extension.dart
@@ -1,13 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
-import '../../non_web.dart' if (dart.library.html) 'package:web/web.dart'
-    as web;
+import 'package:universal_html/html.dart' show window;
 import 'package:universal_platform/universal_platform.dart';
 
 // TODO(Xazin): Refactor to honor `Theme.platform`
 extension PlatformExtension on Platform {
-  static String get _webPlatform => web.window.navigator.platform.toLowerCase();
+  static String get _webPlatform =>
+      window.navigator.platform?.toLowerCase() ?? '';
 
   /// Returns true if the operating system is macOS and not running on Web platform.
   static bool get isMacOS => UniversalPlatform.isMacOS;
